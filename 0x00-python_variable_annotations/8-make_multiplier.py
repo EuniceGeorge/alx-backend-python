3ea774a088fb8bdd33250e3bd3d33a15fc128e51
EuniceGeorge <georgeunice408.com>
2024-10-11 11:46:11 +0100

    update

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,5 +7,6 @@ from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """callable function"""
     def mul(x: float) -> float:
+        ""'mul""'
         return x * multiplier
     return mul    
