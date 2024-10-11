590c5fe346d09e78002ff29aca661faa724dd575
EuniceGeorge <georgeunice408.com>
2024-10-11 11:06:54 +0100

    task 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type_annotated function"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """str_kv"""
+    squared = v ** 2
+    return (k, int(squared) if isinstance(v, int) else squared)
