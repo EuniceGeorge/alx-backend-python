d5975e19ddd08c79c1fb7b633b5a9a4a5cdfe4e0
EuniceGeorge <georgeunice408.com>
2024-10-16 11:42:21 +0100

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Test file for printing the correct output of the wait_n coroutine"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n"""
+    task = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*task)
+    return (result)
