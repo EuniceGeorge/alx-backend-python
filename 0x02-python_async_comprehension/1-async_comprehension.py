2b780f964ab9175c8ca12ad64f1ae1914d5d939a
EuniceGeorge <georgeunice408.com>
2024-10-16 14:34:53 +0100

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,10 +3,10 @@
 
 
 import asyncio
-from typing import List
+from typing import AsyncGenerator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> AsyncGenerator[float, None]:
     """async_comprehension"""
     return [number async for number in async_generator()]
