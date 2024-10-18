2aba53c323550ee5222cd55f9944b1e37dc21b1b
EuniceGeorge <georgeunice408.com>
2024-10-18 11:51:44 +0100

    task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """async_generator"""
 
     for _ in range(10):
