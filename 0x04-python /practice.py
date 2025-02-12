2c30e40bbb8f38599b072a2eb8ee54bafd1e171e
EuniceGeorge <georgeunice408.com>
2025-02-12 12:44:14 +0100

    task

---
+++ 0x04-python /practice.py
@@ -0,0 +1,18 @@
+# print("my name is Eunice")
+# print("same",
+#       "message",
+#       "As before")
+# print("here", end=" ")
+# print("it is....")
+
+# Write a Tipper program where the user enters a restaurant
+# bill total. The program should then display two amounts: a
+# 15 percent tip and a 20 percent tip.
+
+print("Your total bill is 400000")
+first = int((400000 * 15) // 100)
+second = int ((400000 * 20 )// 100)
+
+print("for a 15% discount, you will pay", first)
+
+print("for a 20% discount, you will pay ", second)
